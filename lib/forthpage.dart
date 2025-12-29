dd2b4df6548b01a732005ed095168b53f3ebd083
abhishek <abhi095sharma@gmail.com>
2025-12-29 15:58:00 +0530

    Full project upload

--- lib/forthpage.dart
+++ lib/forthpage.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class Forthpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Hey"));
+    return Scaffold(
+      appBar: AppBar(title: Text("forth page")),
+      body: Column(children: [Text("Hey")]),
+    );
   }
 }
